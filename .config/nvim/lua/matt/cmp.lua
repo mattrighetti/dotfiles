a24ef4e8a55a6625f45868d9f1705b92873cb5df
Mattia Righetti <matt95.righetti@gmail.com>
2023-08-02 15:11:55 +0200

    fix: nvim cmp

--- .config/nvim/lua/matt/cmp.lua
+++ .config/nvim/lua/matt/cmp.lua
@@ -18,8 +18,6 @@ cmp.setup {
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
       else
         fallback()
       end
@@ -27,16 +25,16 @@ cmp.setup {
     ['<S-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
       else
         fallback()
       end
     end, { 'i', 's' }),
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    {name = 'path'},
+    {name = 'nvim_lsp', keyword_length = 1},
+    {name = 'buffer', keyword_length = 3},
+    {name = 'luasnip', keyword_length = 2},
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
